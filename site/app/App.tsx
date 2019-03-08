56ec6bd6b3a4b8e0e9e4be9cdbc80d817fcfd459
Di Wang <wangdicoder@gmail.com>
2019-03-08 17:24:35 +1100

    dev: basic button function

--- site/app/App.tsx
+++ site/app/App.tsx
@@ -3,11 +3,20 @@
  */
 import * as React from 'react';
 import {Button} from '../../src';
+import {IoIosAdd } from 'react-icons/io';
 
 const App = (props: any) => {
     return (
         <div>
-            <Button>Click</Button>
+            <Button size="xs" color="pink" round>Primary Button</Button>
+            <Button link size="xs" color="pink" round>Primary Button</Button>
+            <Button disabled ripple size="sm" color="pink">Primary Button</Button>
+            <Button ripple size="sm" color="yellow" round link dash>Primary Button</Button>
+            <Button ripple size="sm" color="yellow" round outline dash>Primary Button</Button>
+            <Button ripple color="pink" dash onClick={() => console.log('dasdasd')}>Primary Button</Button>
+            <Button size="lg" color="pink" block round>Primary Button</Button>
+            <Button color="pink" round outline icon={<IoIosAdd />}>Primary Button</Button>
+            <Button size="lg" color="pink" loading outline disabled>Primary Button</Button>
         </div>
     );
 };
--- src/button/button.tsx
+++ src/button/button.tsx
@@ -1,23 +1,63 @@
 import * as React from 'react';
 import './style/index.css';
+import classNames from 'classnames';
+import {IoIosSync} from 'react-icons/io';
 
 export interface ButtonProps {
+    color?: string;
     loading?: boolean;
     disabled?: boolean;
-    type?: string;
+    block?: boolean;
+    outline?: boolean;
     size?: string;
-    onClick?: React.MouseEventHandler<HTMLAnchorElement>;
+    round?: boolean;
+    ripple?: boolean;
+    dash?: boolean;
+    icon?: React.ReactNode;
+    link?: boolean;
+
+    // Allow to pass html attribute
+    [others: string]: any;
 }
 
-const Button: React.FC<ButtonProps> = (props) => {
+const renderIcon = (icon: React.ReactNode, loading: boolean = false) => {
+    if (loading) {
+        return <IoIosSync className="ty-btn__loader"/>;
+    } else {
+        return icon;
+    }
+};
 
+const Button: React.FC<ButtonProps> = (props) => {
+    const {color, size, block, icon, link, loading, dash, disabled, outline,
+        round, ripple, children, ...others} = props;
+    const className = classNames(
+        'ty-btn',
+        link ? [`ty-btn_${color}_link`] : (outline ? [`ty-btn_${color}_outline`] : [`ty-btn_${color}`]),
+        size && `ty-btn_${size}`,
+        {'ty-btn_block': block},
+        {'ty-btn_round': round},
+        {'ty-btn_ripple': ripple},
+        {'ty-btn_dash': outline && dash},  // Only available outline style
+        {[`ty-btn_${color}_disabled`]: disabled || loading},
+    );
     return (
         <button
-            className="ty-button"
-            disabled={props.disabled}>
-            {props.children}
+            className={className}
+            disabled={disabled || loading}
+            type="button"
+            {...others}>
+            {renderIcon(icon, loading)}
+            {children}
         </button>
     );
+};
+
+Button.defaultProps = {
+    loading: false,
+    disabled: false,
+    block: false,
+    color: 'basic',
 };
 
 export default Button;
