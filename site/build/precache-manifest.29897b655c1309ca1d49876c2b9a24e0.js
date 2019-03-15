56a24cfa447239afdb4d7650744375fcc83cb3ad
Di Wang <wangdicoder@gmail.com>
2019-03-16 10:46:23 +1100

    dev: configure site

---
+++ site/build/precache-manifest.29897b655c1309ca1d49876c2b9a24e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c93cd997ff648aca788b",
+    "url": "/static/js/main.0cf3c204.chunk.js"
+  },
+  {
+    "revision": "2ecb362ea49c5f878310",
+    "url": "/static/js/2.752b4a37.chunk.js"
+  },
+  {
+    "revision": "c93cd997ff648aca788b",
+    "url": "/static/css/main.4567101e.chunk.css"
+  },
+  {
+    "revision": "3af1d442e3482a1ba32566251405ab2a",
+    "url": "/index.html"
+  }
+];
