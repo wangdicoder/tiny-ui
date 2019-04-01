9dbc9ebeaba8d019b9c6d37316105d180ab33220
Di Wang <wangdicoder@gmail.com>
2019-04-01 11:54:25 +1100

    dev: fix markdown viewer bug

--- site/src/docs/component/button/index.md
+++ site/src/docs/component/button/index.md
@@ -45,8 +45,8 @@ Alert.error(content: string, duration?: number, onClose?: () => void);
 
 _props description_
 
-| Property   | Type`(Default)` | Description                                                                                               |
-| ---------- | --------------- | --------------------------------------------------------------------------------------------------------- |
+| Property   | Type`(Default)` | Description |
+| ---------- | --------------- | ----------- |
 | content \* | string          | The content of alert                                                                                      |
 | duration   | number `(2000)` | The duration of the display, automatically closes the reminder box after the duration (unit:milliseconds) |
 | onClose    | ()=>void        | Callback when Alert is closed                                                                             |
--- site/src/lib/MarkdownViewer.js
+++ site/src/lib/MarkdownViewer.js
@@ -2,21 +2,20 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
 const HEADINGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-const renderingGenerator = (props, type, className) => React.createElement(type, { ...props, className });
+const renderingGenerator = (props, type, className) => React.createElement(type, { children: props.children, className });
 
 const MarkdownViewer = ({ content }) => {
-    return (
-        <ReactMarkdown
-            source={content}
-            renderers={{
-                link: props => renderingGenerator(props, 'a', 'code-link'),
-                heading: props => renderingGenerator(props, HEADINGS[props.level - 1], `code-heading-${props.level}`),
-                paragraph: props => renderingGenerator(props, 'p', 'code-p'),
-                table: props => <div
-                    className="code-table-container">{renderingGenerator(props, 'table', 'code-table')}</div>,
-            }}
-        />
-    );
+	return (
+		<ReactMarkdown
+			source={content}
+			renderers={{
+				link: props => renderingGenerator(props, 'a', 'code-link'),
+				heading: props => renderingGenerator(props, HEADINGS[props.level - 1], `code-heading-${props.level}`),
+				paragraph: props => renderingGenerator(props, 'p', 'code-p'),
+				table: props => <div className="code-table-container">{renderingGenerator(props, 'table', 'code-table')}</div>
+			}}
+		/>
+	);
 };
 
 export default MarkdownViewer;
