5834d4b2209b8356fade3eaffa64d5feae1e848e
Di Wang <wangdicoder@gmail.com>
2019-11-19 02:14:58 +1100

    popover: event listener

--- components/popover/index.tsx
+++ components/popover/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import { BaseProps } from '../_utils/props';
 import Popup, { PlacementType } from '../popup';
@@ -14,6 +14,7 @@ export interface PopoverProps extends BaseProps {
   children: React.ReactElement;
 }
 
+let eventTarget: EventTarget | null = null;
 const Popover = ({
   prefixCls = 'ty-popover',
   placement = 'top-center',
@@ -21,74 +22,93 @@ const Popover = ({
   ...restProps
 }: PopoverProps) => {
   const { title, content, className, children } = restProps;
-  const [showPopup, setShowPopup] = useState(false);
+  const [popupVisible, setPopupVisible] = useState(false);
   const cls = classNames(className, prefixCls);
   const [target, setTarget] = useState<HTMLElement | undefined>(undefined);
   const popupRef = useRef<HTMLDivElement | null>(null);
 
-  const handlePopupMouseOver = () => {
-    trigger === 'hover' && setShowPopup(true);
+  const displayPopup = useCallback(() => {
+    setPopupVisible(true);
+  }, []);
+
+  const hidePopup = useCallback(() => {
+    setPopupVisible(false);
+  }, []);
+
+  const handlePopupMouseOver = (): void => {
+    trigger === 'hover' && setPopupVisible(true);
   };
 
-  const handlePopupMouseOut = () => {
-    trigger === 'hover' && setShowPopup(false);
+  const handlePopupMouseOut = (): void => {
+    trigger === 'hover' && setPopupVisible(false);
   };
 
-  const handleClickOutside = (e: Event): void => {
-    if (
-      (popupRef.current && !popupRef.current.contains(e.target as HTMLElement)) ||
-      (showPopup && target && !target.contains(e.target as HTMLElement))
-    ) {
-      setShowPopup(false);
+  const isInPopup = (): boolean => {
+    const eventEl = eventTarget as HTMLElement;
+    const flag = (popupRef.current as HTMLDivElement).contains(eventEl);
+    eventTarget = null;
+    return flag;
+  };
+
+  const handleClickOutside = useCallback(
+    (e: Event): void => {
+      eventTarget = e.target;
+      if (isInPopup()) return;
+
+      hidePopup();
       document.removeEventListener('click', handleClickOutside);
-    }
-  };
+    },
+    [popupVisible]
+  );
 
-  const handleClick = (e: Event): void => {
-    e.preventDefault();
-    if (showPopup) {
-      setShowPopup(false);
-      document.removeEventListener('click', handleClickOutside);
-    } else {
-      setShowPopup(true);
-      document.addEventListener('click', handleClickOutside);
-    }
-  };
+  const handleClick = useCallback(
+    (e: Event): void => {
+      e.preventDefault();
+      if (popupVisible) {
+        hidePopup();
+      } else {
+        displayPopup();
+        document.addEventListener('click', handleClickOutside);
+        e.stopPropagation();
+      }
+    },
+    [handleClickOutside]
+  );
 
   useEffect(() => {
     if (!target) return undefined;
 
     if (trigger === 'hover') {
-      target.addEventListener('mouseenter', () => {
-        setShowPopup(true);
-      });
-      target.addEventListener('mouseleave', () => {
-        setShowPopup(false);
-      });
+      target.addEventListener('mouseenter', displayPopup);
+      target.addEventListener('mouseleave', hidePopup);
     } else if (trigger === 'click') {
       target.addEventListener('click', handleClick);
+    } else if (trigger === 'context-menu') {
+      target.addEventListener('contextmenu', handleClick);
+    } else {
+      target.addEventListener('focus', displayPopup);
+      target.addEventListener('blur', hidePopup);
     }
 
     return () => {
-      target.removeEventListener('mouseenter', () => {
-        setShowPopup(true);
-      });
-      target.removeEventListener('mouseleave', () => {
-        setShowPopup(false);
-      });
+      target.removeEventListener('mouseenter', displayPopup);
+      target.removeEventListener('mouseleave', hidePopup);
       target.removeEventListener('click', handleClick);
+      target.removeEventListener('contextmenu', handleClick);
+      target.removeEventListener('focus', displayPopup);
+      target.removeEventListener('blur', hidePopup);
     };
-  }, [target]);
+  }, [target, displayPopup, hidePopup, handleClick]);
 
   if (children) {
     return (
       <>
         {React.cloneElement(React.Children.only(children), {
-          ref: (el: any) => setTarget(el),
+          ref: (el: HTMLElement) => setTarget(el),
         })}
         <Popup
           target={target}
-          show={showPopup}
+          show={popupVisible}
           placement={placement}
           onMouseOver={handlePopupMouseOver}
           onMouseOut={handlePopupMouseOut}>
