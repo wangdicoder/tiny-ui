71607038b7f0f7a6ee466c22e11229c029f0c6fa
Di Wang <wangdicoder@gmail.com>
2019-11-19 11:26:11 +1100

    layout: fix sidebar style

--- site/src/components/header/index.tsx
+++ site/src/components/header/index.tsx
@@ -4,40 +4,37 @@ import { NavLink } from 'react-router-dom';
 import { version, repository } from '../../../../package.json';
 
 const Header = () => (
-  <>
-    <div className="header__placeholder" />
-    <header className="header">
-      <NavLink to="/home" className="header__link">
-        <div className="header__logo">
-          <img src={require('../../assets/logo/logo.svg')} alt="logo" width={35} />
-          <span className="header__title">Tiny UI</span>
-        </div>
-      </NavLink>
-      <nav className="header__nav">
-        <ul className="header__nav-list">
-          <li className="header__nav-item">
-            <NavLink to="/home" className="header__link">
-              Home
-            </NavLink>
-          </li>
-          <li className="header__nav-item">
-            <NavLink to="/components" className="header__link">
-              Components
-            </NavLink>
-          </li>
-          <li className="header__nav-item">
-            <a
-              href={repository.url}
-              target="_blank"
-              className="header__link"
-              rel="noreferrer noopener">
-              <span className="header__version">v{version}</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  </>
+  <header className="header">
+    <NavLink to="/home" className="header__link">
+      <div className="header__logo">
+        <img src={require('../../assets/logo/logo.svg')} alt="logo" width={35} />
+        <span className="header__title">Tiny UI</span>
+      </div>
+    </NavLink>
+    <nav className="header__nav">
+      <ul className="header__nav-list">
+        <li className="header__nav-item">
+          <NavLink to="/home" className="header__link">
+            Home
+          </NavLink>
+        </li>
+        <li className="header__nav-item">
+          <NavLink to="/components" className="header__link">
+            Components
+          </NavLink>
+        </li>
+        <li className="header__nav-item">
+          <a
+            href={repository.url}
+            target="_blank"
+            className="header__link"
+            rel="noreferrer noopener">
+            <span className="header__version">v{version}</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 );
 
 export default Header;
--- site/src/components/sidebar-menu/index.tsx
+++ site/src/components/sidebar-menu/index.tsx
@@ -15,46 +15,48 @@ const SidebarMenu = (): React.ReactElement => {
   });
   return (
     <Sidebar theme="light" width={250} className="sidebar-menu">
-      <h4 className="sidebar-menu__title">Guide</h4>
-      <ul className="sidebar-menu__menu">
-        {GUIDE_MENU.map((menu, idx) => (
-          <li key={idx} className="sidebar-menu__menu-item">
-            <NavLink
-              to={{
-                pathname: `/components/${menu.route}`,
-                state: { name: menu.title },
-              }}
-              activeClassName="sidebar-menu__menu-item_active">
-              {menu.title}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
+      <div className="sidebar-menu__wrapper">
+        <h4 className="sidebar-menu__title">Guide</h4>
+        <ul className="sidebar-menu__menu">
+          {GUIDE_MENU.map((menu, idx) => (
+            <li key={idx} className="sidebar-menu__menu-item">
+              <NavLink
+                to={{
+                  pathname: `/components/${menu.route}`,
+                  state: { name: menu.title },
+                }}
+                activeClassName="sidebar-menu__menu-item_active">
+                {menu.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
 
-      <h4 className="sidebar-menu__title">
-        Components <span className="sidebar-menu__count-label">({totalComponents})</span>
-      </h4>
-      <ul className="sidebar-menu__menu">
-        {COMPONENT_MENU.map((mainMenu, idx) => (
-          <React.Fragment key={idx}>
-            <li className="sidebar-menu__group-title">{mainMenu.title}</li>
-            <ul className="sidebar-menu__group-list">
-              {mainMenu.children!.map((menu, idx) => (
-                <li key={idx} className="sidebar-menu__menu-item">
-                  <NavLink
-                    to={{
-                      pathname: `/components/${menu.route}`,
-                      state: { name: menu.title },
-                    }}
-                    activeClassName="sidebar-menu__menu-item_active">
-                    {menu.title}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </React.Fragment>
-        ))}
-      </ul>
+        <h4 className="sidebar-menu__title">
+          Components <span className="sidebar-menu__count-label">({totalComponents})</span>
+        </h4>
+        <ul className="sidebar-menu__menu">
+          {COMPONENT_MENU.map((mainMenu, idx) => (
+            <React.Fragment key={idx}>
+              <li className="sidebar-menu__group-title">{mainMenu.title}</li>
+              <ul className="sidebar-menu__group-list">
+                {mainMenu.children!.map((menu, idx) => (
+                  <li key={idx} className="sidebar-menu__menu-item">
+                    <NavLink
+                      to={{
+                        pathname: `/components/${menu.route}`,
+                        state: { name: menu.title },
+                      }}
+                      activeClassName="sidebar-menu__menu-item_active">
+                      {menu.title}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </React.Fragment>
+          ))}
+        </ul>
+      </div>
     </Sidebar>
   );
 };
