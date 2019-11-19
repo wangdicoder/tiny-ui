afbed1945dd7b23f3f7d46980a1ae79c47d83a64
Di Wang <wangdicoder@gmail.com>
2019-11-19 20:43:44 +1100

    feature: add tooltip component

--- components/tooltip/index.tsx
+++ components/tooltip/index.tsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { BaseProps } from '../_utils/props';
-import Popover from '../popover';
+import Popover, { TriggerType } from '../popover';
+import { PlacementType } from '../popup';
 
 export interface TooltipProps extends BaseProps {
   title?: string | React.ReactNode;
+  placement?: PlacementType;
+  trigger?: TriggerType;
   visible?: boolean;
+  defaultVisible?: boolean;
+  onVisibleChange?: (visible: boolean) => void;
   children: React.ReactElement;
 }
 
-const Tooltip = ({ visible = false, ...restProps }: TooltipProps) => {
-  const { title, className, prefixCls, children } = restProps;
-  const cls = classNames(prefixCls, className);
+const Tooltip = ({
+  defaultVisible = false,
+  placement = 'right-center',
+  trigger = 'hover',
+  prefixCls = 'ty-tooltip',
+  ...restProps
+}: TooltipProps) => {
+  const { title, visible, onVisibleChange, className, style, children } = restProps;
+  const cls = classNames(prefixCls, className, `${prefixCls}_${placement}`);
+  const [popupVisible, setPopupVisible] = useState(
+    'visible' in restProps ? visible : defaultVisible
+  );
+
+  useEffect(() => {
+    'visible' in restProps && setPopupVisible(restProps.visible);
+  }, [restProps.visible]);
 
   return (
-    <Popover theme="dark" className={cls} content={title}>
+    <Popover
+      theme="dark"
+      gap={-2}
+      onVisibleChange={onVisibleChange}
+      visible={popupVisible}
+      placement={placement}
+      className={cls}
+      style={style}
+      trigger={trigger}
+      content={title}>
       {children}
     </Popover>
   );
