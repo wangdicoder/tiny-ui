115c39ba435d57257f56e5e907775cc6da7bb216
Di Wang <wangdicoder@gmail.com>
2019-11-25 10:43:41 +1100

    add commitlint

--- .cz-config.js
+++ .cz-config.js
@@ -41,14 +41,14 @@ module.exports = {
       name: '💪  WIP:      Work in progress',
     },
   ],
-
+  
   scopes: [{ name: 'lib' }, { name: 'example' }, { name: 'site' }],
-
+  
   allowTicketNumber: false,
   isTicketNumberRequired: false,
   ticketNumberPrefix: 'TICKET-',
   ticketNumberRegExp: '\\d{1,5}',
-
+  
   // it needs to match the value for field type. Eg.: 'fix'
   /*
   scopeOverrides: {
@@ -72,15 +72,15 @@ module.exports = {
     footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
     confirmCommit: 'Are you sure you want to proceed with the commit above?',
   },
-
+  
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
   // skip any questions you want
   skipQuestions: ['body'],
-
+  
   // limit subject length
   subjectLimit: 100,
   // breaklineChar: '|', // It is supported for fields body and footer.
   // footerPrefix : 'ISSUES CLOSED:'
   // askForBreakingChangeFirst : true, // default is false
-}
+};
