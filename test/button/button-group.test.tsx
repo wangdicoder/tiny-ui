9fa21f33c8d3b535fd4b4768b6d0a1103077145d
Di Wang <wangdicoder@gmail.com>
2019-12-03 23:20:30 +1100

    test(lib): fix button test

--- test/button/button-group.test.tsx
+++ test/button/button-group.test.tsx
@@ -1,17 +1,25 @@
-import * as React from 'react';
-import {render, mount} from 'enzyme';
-import Button from './';
+import React from 'react';
+import { render, mount } from 'enzyme';
+import Button from '../../components/button';
 
-const {Group} = Button;
+const { Group } = Button;
 
 describe('<Button />', () => {
-    it('should match snapshot', () => {
-        const wrapper = render(<Group><Button>Default</Button></Group>);
-        expect(wrapper).toMatchSnapshot();
-    });
+  it('should match snapshot', () => {
+    const wrapper = render(
+      <Group>
+        <Button>Default</Button>
+      </Group>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
 
-    it('should render child button correctly', () => {
-        const wrapper = mount(<Group color="primary"><Button>Default</Button></Group>);
-        expect(wrapper.childAt(0).find('.ty-btn_primary').length).toBe(1);
-    });
+  it('should render child button correctly', () => {
+    const wrapper = mount(
+      <Group color="primary">
+        <Button>Default</Button>
+      </Group>
+    );
+    expect(wrapper.childAt(0).find('.ty-btn_primary').length).toBe(1);
+  });
 });
--- test/form/form-store.test.tsx
+++ test/form/form-store.test.tsx
@@ -1,19 +1,17 @@
-import * as React from 'react';
 import FormStore from '../../components/form/form-store';
 
 describe('FormStore', () => {
+  it('set', () => {
+    const store = new FormStore();
 
-    it('set', () => {
-        const store = new FormStore();
+    store.setFieldValue('key', 'value');
+    expect(store.getFieldValue('key')).toBe('value');
 
-        store.setFieldValue('key', 'value');
-        expect(store.getFieldValue('key')).toBe('value');
+    store.setFieldValues({ a: 1, b: 2 });
+    expect(store.getFieldValue('a')).toBe(1);
+    expect(store.getFieldValue('b')).toBe(2);
 
-        store.setFieldsValue({ a: 1, b: 2 });
-        expect(store.getFieldValue('a')).toBe(1);
-        expect(store.getFieldValue('b')).toBe(2);
-
-        store.setFieldValue('this.is.a.deep.key', 'value');
-        expect(store.getFieldValue('this.is.a.deep.key')).toBe('value');
-    });
+    store.setFieldValue('this.is.a.deep.key', 'value');
+    expect(store.getFieldValue('this.is.a.deep.key')).toBe('value');
+  });
 });
