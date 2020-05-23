b9bf10623ed512ac5c42cb1a0e9e4646015fcadf
wangdi <wangdicoder@gmail.com>
2020-05-23 19:06:47 +1000

    fix: `<Switch />` onClick event

--- components/switch/index.tsx
+++ components/switch/index.tsx
@@ -4,63 +4,69 @@ import { BaseProps, SizeType } from '../_utils/props';
 import { ConfigContext } from '../config-provider/config-context';
 import { getPrefixCls } from '../_utils/general';
 
-export interface SwitchProps extends BaseProps {
+export interface SwitchProps
+  extends BaseProps,
+    Omit<React.PropsWithRef<JSX.IntrinsicElements['label']>, 'onChange' | 'onClick'> {
   defaultChecked?: boolean;
   checked?: boolean;
   disabled?: boolean;
   loading?: boolean;
   size?: SizeType;
   onChange?: (checked: boolean, event: React.MouseEvent) => void;
+  onClick?: (checked: boolean, event: React.MouseEvent) => void;
   checkedText?: React.ReactNode;
   uncheckedText?: React.ReactNode;
-  prefixCls?: string;
-  className?: string;
-  style?: React.CSSProperties;
-  children?: React.ReactNode;
 }
 
-const Switch = (props: SwitchProps): JSX.Element => {
-  const {
-    size = 'md',
-    defaultChecked = true,
-    prefixCls: customisedCls,
-    disabled,
-    loading,
-    onChange,
-    checkedText,
-    uncheckedText,
-    className,
-    style,
-  } = props;
-  const [checked, setChecked] = useState('checked' in props ? props.checked : defaultChecked);
-  const configContext = useContext(ConfigContext);
-  const prefixCls = getPrefixCls('switch', configContext.prefixCls, customisedCls);
-  const switchSize = props.size || configContext.componentSize || size;
-  const cls = classNames(prefixCls, className, `${prefixCls}_${switchSize}`, {
-    [`${prefixCls}_checked`]: checked,
-    [`${prefixCls}_loading`]: loading,
-    [`${prefixCls}_disabled`]: loading || disabled,
-  });
+const Switch = React.forwardRef<HTMLLabelElement, SwitchProps>(
+  (props: SwitchProps, ref): JSX.Element => {
+    const {
+      size = 'md',
+      defaultChecked = true,
+      disabled,
+      loading,
+      onChange,
+      checkedText,
+      uncheckedText,
+      className,
+      onClick,
+      prefixCls: customisedCls,
+      ...otherProps
+    } = props;
+    const [checked, setChecked] = useState('checked' in props ? props.checked : defaultChecked);
+    const configContext = useContext(ConfigContext);
+    const prefixCls = getPrefixCls('switch', configContext.prefixCls, customisedCls);
+    const switchSize = props.size || configContext.componentSize || size;
+    const cls = classNames(prefixCls, className, `${prefixCls}_${switchSize}`, {
+      [`${prefixCls}_checked`]: checked,
+      [`${prefixCls}_loading`]: loading,
+      [`${prefixCls}_disabled`]: loading || disabled,
+    });
 
-  const onClick = (e: React.MouseEvent<HTMLSpanElement>): void => {
-    if (!(disabled || loading)) {
-      onChange && onChange(!checked, e);
-      !('checked' in props) && setChecked(!checked);
-    }
-  };
+    const labelOnClick = (e: React.MouseEvent<HTMLLabelElement>): void => {
+      const val = !checked;
+      onClick && onClick(val, e);
+      if (!(disabled || loading)) {
+        onChange && onChange(val, e);
+        !('checked' in props) && setChecked(val);
+      }
+    };
 
-  useEffect(() => {
-    'checked' in props && setChecked(props.checked);
-  }, [props]);
+    useEffect(() => {
+      'checked' in props && setChecked(props.checked);
+    }, [props]);
 
-  return (
-    <label className={cls} style={style} onClick={onClick}>
-      <span className={`${prefixCls}__bg`}>
-        <span className={`${prefixCls}__thumb`} />
-        <span className={`${prefixCls}__label`}>{checked ? checkedText : uncheckedText}</span>
-      </span>
-    </label>
-  );
-};
+    return (
+      <label {...otherProps} ref={ref} className={cls} onClick={labelOnClick}>
+        <span className={`${prefixCls}__bg`}>
+          <span className={`${prefixCls}__thumb`} />
+          <span className={`${prefixCls}__label`}>{checked ? checkedText : uncheckedText}</span>
+        </span>
+      </label>
+    );
+  }
+);
+
+Switch.displayName = 'Switch';
 
 export default Switch;
