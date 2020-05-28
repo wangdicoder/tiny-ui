c98a091b33b5a476115d0bb3f193c5fc3cb4f453
wangdi <wangdicoder@gmail.com>
2020-05-28 11:07:37 +1000

    WIP: Transfer - check list

--- components/transfer/transfer-panel.tsx
+++ components/transfer/transfer-panel.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useContext } from 'react';
+import React, { ChangeEvent, ReactNode, useContext } from 'react';
 import classNames from 'classnames';
 import { ConfigContext } from '../config-provider/config-context';
 import { getPrefixCls } from '../_utils/general';
@@ -6,54 +6,89 @@ import { BaseProps } from '../_utils/props';
 import { TransferItem } from './transfer';
 import Checkbox from '../checkbox/checkbox';
 import CheckboxGroup from '../checkbox/checkbox-group';
+import Empty from '../empty';
 
 export interface TransferPanelProps
   extends BaseProps,
-    Omit<React.PropsWithRef<JSX.IntrinsicElements['div']>, 'title'> {
+    Omit<React.PropsWithRef<JSX.IntrinsicElements['div']>, 'title' | 'onChange'> {
   dataSource: TransferItem[];
+  checkedKeys: string[];
+  onChange: (checkedKeys: string[]) => void;
+  disabled: boolean;
   title?: ReactNode;
   footer?: ReactNode;
 }
 
 const TransferPanel = React.forwardRef<HTMLDivElement, TransferPanelProps>(
   (props: TransferPanelProps, ref): React.ReactElement => {
-    const { dataSource, title, footer, className, prefixCls: customisedCls, ...otherProps } = props;
+    const {
+      dataSource,
+      checkedKeys,
+      title,
+      footer,
+      className,
+      onChange,
+      prefixCls: customisedCls,
+      ...otherProps
+    } = props;
     const configContext = useContext(ConfigContext);
     const prefixCls = getPrefixCls('transfer-panel', configContext.prefixCls, customisedCls);
     const cls = classNames(prefixCls, className);
+    const checkableData = dataSource.filter((item) => !item.disabled);
 
     const allChecked = (): boolean => {
-      return true;
+      return checkedKeys.length === checkableData.length;
     };
 
-    const handleAllCheckedChange = () => {};
+    /**
+     * Footer checkbox onChange event
+     */
+    const handleAllCheckedChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const isChecked = e.currentTarget.checked;
+      const checkedKeys = isChecked ? checkableData.map((item) => item.key) : [];
+      onChange(checkedKeys);
+    };
 
     const isIndeterminate = (): boolean => {
-      return false;
+      const { length } = checkedKeys;
+      return length > 0 && length < checkableData.length;
     };
 
-    const checkedSummary = () => {};
+    const checkedSummary = (): string => {
+      return `${dataSource.length} items`;
+    };
 
     return (
       <div {...otherProps} className={cls} ref={ref}>
         <div className={`${prefixCls}__header`}>{title}</div>
         <div className={`${prefixCls}__body`}>
-          <CheckboxGroup>
-            {dataSource.map(({ key, label, disabled }) => {
-              return (
-                <Checkbox key={key} disabled={disabled}>
-                  {label}
-                </Checkbox>
-              );
-            })}
-          </CheckboxGroup>
+          {dataSource.length > 0 ? (
+            <CheckboxGroup
+              value={checkedKeys}
+              onChange={(values) => onChange(values)}
+              className={`${prefixCls}__container`}>
+              <ul className={`${prefixCls}__list-content`}>
+                {dataSource.map(({ key, label, disabled }) => {
+                  return (
+                    <li key={key} className={`${prefixCls}__list-item`}>
+                      <Checkbox value={key} disabled={disabled}>
+                        {label}
+                      </Checkbox>
+                    </li>
+                  );
+                })}
+              </ul>
+            </CheckboxGroup>
+          ) : (
+            <Empty className={`${prefixCls}__not-found`} />
+          )}
         </div>
         <div className={`${prefixCls}__footer`}>
           <Checkbox
             checked={allChecked()}
             onChange={handleAllCheckedChange}
             indeterminate={isIndeterminate()}>
-            {checkedSummary}
+            {checkedSummary()}
           </Checkbox>
           {footer}
         </div>
--- components/transfer/transfer.tsx
+++ components/transfer/transfer.tsx
@@ -17,23 +17,26 @@ export interface TransferProps
   extends BaseProps,
     Omit<React.PropsWithRef<JSX.IntrinsicElements['div']>, 'onChange'> {
   dataSource?: TransferItem[];
-  leftKeys?: string[];
-  rightKeys?: string[];
-  defaultLeftKeys?: string[];
-  defaultRightKeys?: string[];
+  value?: string[];
+  defaultValue?: string[];
   disabled?: boolean;
   titles?: [ReactNode, ReactNode];
   buttonTexts?: [ReactNode, ReactNode];
   onChange?: (targetKeys: string[], direction: string, moveKeys: string[]) => void;
+  leftDefaultChecked?: string[];
+  rightDefaultChecked?: string[];
 }
 
 const Transfer = React.forwardRef<HTMLDivElement, TransferProps>(
   (props: TransferProps, ref): React.ReactElement => {
     const {
       dataSource = [],
-      defaultLeftKeys = [],
-      defaultRightKeys = [],
-      buttonTexts,
+      defaultValue = [],
+      buttonTexts = [],
+      titles = [],
+      leftDefaultChecked = [],
+      rightDefaultChecked = [],
+      disabled = false,
       className,
       onChange,
       prefixCls: customisedCls,
@@ -42,44 +45,57 @@ const Transfer = React.forwardRef<HTMLDivElement, TransferProps>(
     const configContext = useContext(ConfigContext);
     const prefixCls = getPrefixCls('transfer', configContext.prefixCls, customisedCls);
     const cls = classNames(prefixCls, className);
-    const [leftChecked, setLeftChecked] = useState(
-      'leftKeys' in props ? (props.leftKeys as string[]) : defaultLeftKeys
-    );
-    const [rightChecked, setRightChecked] = useState(
-      'rightKeys' in props ? (props.rightKeys as string[]) : defaultRightKeys
-    );
+
+    const getDataKeys = () => {
+      const rightKeys: string[] = 'value' in props ? (props.value as string[]) : defaultValue;
+      const rightData = dataSource.filter((item) => rightKeys.includes(item.key));
+      const leftData = dataSource.filter((item) => !rightKeys.includes(item.key));
+      return [leftData, rightData];
+    };
+    const [leftData, rightData] = getDataKeys();
+    const [sourceData] = useState(leftData);
+    const [targetData] = useState(rightData);
+    const [leftCheckedKeys, setLeftCheckedKeys] = useState(leftDefaultChecked);
+    const [rightCheckedKeys, setRightCheckedKeys] = useState(rightDefaultChecked);
 
     const addToLeft = () => {};
 
     const addToRight = () => {};
 
-    useEffect(() => {
-      setLeftChecked([]);
-      setRightChecked([]);
-    }, []);
+    useEffect(() => {}, []);
 
     return (
       <div {...otherProps} className={cls} ref={ref}>
-        <TransferPanel dataSource={[]} />
+        <TransferPanel
+          dataSource={sourceData}
+          checkedKeys={leftCheckedKeys}
+          disabled={disabled}
+          onChange={(keys: string[]) => setLeftCheckedKeys(keys)}
+        />
         <div className={`${prefixCls}__buttons`}>
           <Button
             btnType="primary"
             size="sm"
             onClick={addToLeft}
-            disabled={rightChecked.length === 0}>
-            <ArrowDown size={14} className={`${prefixCls}__left-arrow`} />
+            disabled={leftCheckedKeys.length === 0}>
+            <ArrowDown size={12} className={`${prefixCls}__left-arrow`} />
             {buttonTexts && buttonTexts[0] !== undefined && <span>{buttonTexts[0]}</span>}
           </Button>
           <Button
             btnType="primary"
             size="sm"
             onClick={addToRight}
-            disabled={leftChecked.length === 0}>
+            disabled={rightCheckedKeys.length === 0}>
             {buttonTexts && buttonTexts[1] !== undefined && <span>{buttonTexts[1]}</span>}
-            <ArrowDown size={14} className={`${prefixCls}__right-arrow`} />
+            <ArrowDown size={12} className={`${prefixCls}__right-arrow`} />
           </Button>
         </div>
-        <TransferPanel dataSource={[]} />
+        <TransferPanel
+          dataSource={targetData}
+          checkedKeys={rightCheckedKeys}
+          disabled={disabled}
+          onChange={(keys: string[]) => setRightCheckedKeys(keys)}
+        />
       </div>
     );
   }
