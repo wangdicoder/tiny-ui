1f864e1b913c8abe056dfc77c8feb1cce7e4342a
wangdi <wangdicoder@gmail.com>
2020-10-25 15:18:55 +1100

    test: button group test

--- components/button/__tests__/button-group.test.tsx
+++ components/button/__tests__/button-group.test.tsx
@@ -13,12 +13,53 @@ describe('<ButtonGroup />', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
-  // it('should render child button correctly', () => {
-  //   const wrapper = mount(
-  //     <ButtonGroup btnType="primary">
-  //       <Button>Default</Button>
-  //     </ButtonGroup>
-  //   );
-  //   expect(wrapper.childAt(0).find('.ty-btn_primary').length).toBe(1);
-  // });
+  it('should render two buttons', () => {
+    const { container } = render(
+      <ButtonGroup>
+        <Button>Default</Button>
+        <Button>Default</Button>
+      </ButtonGroup>
+    );
+    expect(container.querySelectorAll('.ty-btn_default').length).toBe(2);
+  });
+
+  it('should render correct type of buttons', () => {
+    const { container } = render(
+      <ButtonGroup btnType="primary">
+        <Button>Default</Button>
+        <Button>Default</Button>
+      </ButtonGroup>
+    );
+    expect(container.querySelectorAll('.ty-btn_primary').length).toBe(2);
+  });
+
+  it('should render correct size of buttons', () => {
+    const { container } = render(
+      <ButtonGroup size="lg">
+        <Button>Default</Button>
+        <Button>Default</Button>
+      </ButtonGroup>
+    );
+    expect(container.querySelectorAll('.ty-btn_lg').length).toBe(2);
+  });
+
+  it('should render correct disabled of buttons', () => {
+    const { container } = render(
+      <ButtonGroup disabled>
+        <Button>Default</Button>
+        <Button>Default</Button>
+      </ButtonGroup>
+    );
+    expect(container.querySelectorAll('.ty-btn_disabled').length).toBe(2);
+  });
+
+  it('should only disable one button', () => {
+    const { container } = render(
+      <ButtonGroup>
+        <Button disabled>Default</Button>
+        <Button>Default</Button>
+      </ButtonGroup>
+    );
+    expect(container.querySelectorAll('.ty-btn_disabled').length).toBe(1);
+  });
 });
