8f4122614216136d0f358c0d3502bfdec31697d8
wangdi <wangdicoder@gmail.com>
2020-11-06 23:25:34 +1100

    chore: update webpack config

--- site/config/webpack.common.js
+++ site/config/webpack.common.js
@@ -36,7 +36,7 @@ module.exports = {
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
       {
         test: /\.mdx?$/,
