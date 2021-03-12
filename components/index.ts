bbea5e8417f86cdf95401841bdc2e5ca0503112a
wangdi <wangdicoder@gmail.com>
2021-03-12 15:16:19 +1100

    refactor: update index.ts reference

--- components/index.ts
+++ components/index.ts
@@ -70,5 +70,5 @@ export { default as Transition } from './transition';
 export { default as Tree } from './tree';
 export { default as Typography } from './typography';
 export { default as Upload } from './upload';
-// export { default as Waterfall } from './waterfall';
+
 export { withLocale } from './intl-provider/with-locale';
